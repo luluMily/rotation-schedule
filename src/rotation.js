9723e6717f5f160602179ef8f48b66d8a0fcfc5b
luluMily <x2011bhb@gmail.com>
2018-07-22 16:11:23 -0400

    set up deployment

--- src/rotation.js
+++ src/rotation.js
@@ -1,24 +1,105 @@
 import React, { Component } from 'react';
-import axios from 'axios'
+import axios from 'axios';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+
+// const getItems = count => 
+//     Array.from({ length: count }, (v, k) => k).map(k => ({
+//         id: `item=${k}`,
+//         content: `item ${k}`
+//     }))
+
+// a little function to help us with reordering the result
+const reorder = (list, startIndex, endIndex) => {
+    const result = Array.from(list)
+    const [removed] = result.splice(startIndex, 1)
+    result.splice(endIndex, 0, removed)
+
+    return result
+}
+
+const grid = 8
+
+const getItemStyle = (isDragging, draggableStyle) => ({
+    userSelect: 'none',
+    padding: grid * 2,
+    margin: `0 0 ${grid}px 0`,
+    // change background colour if dragging
+    background: isDragging ? 'lightgreen' : 'grey',
+    // styles we need to apply on draggbles
+    ...draggableStyle
+})
+
+const getListStyle = isDraggingOver => ({
+    background: isDraggingOver ? 'lightblue' : 'lightgrey',
+    padding: grid,
+    width: 250
+})
 
 export default class Rotation extends Component {
     state = {
         error: false,
-        loading: false
+        loading: false,
+        // items: getItems(10),
+        items: [],
     }
     
     async componentDidMount() {
         try {
-            const response = await axios.get('/rotation')
-            console.log(response.data)
+            const res = await axios.get('/rotation')
+            this.setState({ items: res.data.people })
         } catch (error) {
             console.log(error)
         }
     }
+
+    onDragEnd = (result) => {
+        // dropped outside the list
+        if (!result.destination) {
+            return;
+        }
+
+        const items = reorder (
+            this.state.items,
+            result.source.index,
+            result.destination.index
+        )
+
+        this.setState({
+            items
+        })
+    }
     
     render () {
         return (
-            <div>Hello</div>
+            <DragDropContext onDragEnd={this.onDragEnd}>
+                <Droppable droppableId="droppable">
+                {(provided, snapshot) => (
+                    <div 
+                    ref={provided.innerRef}
+                    style={getListStyle(snapshot.isDraggingOver)}
+                    >
+                        {this.state.items.map((item, index) => (
+                            <Draggable key={item.id} draggableId={item.id} index={index}>
+                                {(provided, snapshot) => (
+                                    <div
+                                    ref={provided.innerRef}
+                                    {...provided.draggableProps}
+                                    {...provided.dragHandleProps}
+                                    style={getItemStyle(
+                                        snapshot.isDragging,
+                                        provided.draggableProps.style
+                                    )}
+                                    >
+                                    {item.name}
+                                </div>
+                                )}
+                            </Draggable>
+                        ))}
+                        {provided.placeholder}
+                    </div>
+                )}
+                </Droppable>
+            </DragDropContext>
         )
     }
 }
